3bb02a0a404550af36140df12be0f30c6e1f0df9
SANDRO PEREIRA DOS REIS <08929532845@jpmorgan.sp>
2017-11-08 21:57:18 -0200

    inicio projeto

--- ProjetoEvento/bin/ClassePai/ClassesFilhas/Show.cs
+++ ProjetoEvento/bin/ClassePai/ClassesFilhas/Show.cs
@@ -1,7 +1,29 @@
+using System;
 namespace ProjetoEvento.bin.ClassePai.ClassesFilhas
 {
-    public class Show
+    public class Show : Evento
     {
+        public string Artista { get; set; }
+        public string GeneroMusical { get; set; }
+
+        public Show(Parameters)
+        {
+            
+        }
+
+        public Show(string titulo,string local, int lotacao,string duracao,int classificacao,DateTime data, string artista, string generoMusical)
+        {
+            base.Titulo = titulo;
+            base.Local = local;
+            base.Lotacao = lotacao;
+            base.Duracao = duracao;
+            base.Classificacao = classificacao;
+            base.Data = data;
+            this.Artista = artista;
+            this.GeneroMusical = generoMusical;
+            
+        }
         
+                
     }
 }
--- ProjetoEvento/bin/ClassePai/Evento.cs
+++ ProjetoEvento/bin/ClassePai/Evento.cs
@@ -1,7 +1,37 @@
+using System;
 namespace ProjetoEvento.bin.ClassePai
 {
-    public class Evento
-    {
+    public abstract class Evento{
+        public string Titulo { get; set; }
+        public string Local { get; set; }
+        public int Lotacao { get; set; }
+        public string Duracao { get; set; }
+        public DateTime Data { get; set; }
+        public int Classificacao { get; set; }
+
+
+        
+        
+        
+        
+        public virtual bool Cadastrar(){
+
+            return false;
+        }
+
+
+        public virtual string Pesquisar(DateTime dataevento){
+
+            return null;
+        }
+
+        public virtual string Pesquisar(string tituloEvento){
+            
+            return null;
+        }
+
+
+
         
     }
 }
